a4257f7944dc02ee407c210a7f9a64f2f56aca25
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-08-02 12:53:00 -0300

    adicao correta do tempalte e tentando fzr questao das strings

--- STL/game_with_str.cpp
+++ STL/game_with_str.cpp
@@ -2,31 +2,57 @@
 
 using namespace std;
 
-int main(){
-    string s; cin >> s;
+int main()
+{
+    string s;
+    cin >> s;
     int player = 1;
-    while (s.length() > 1){
+    while (s.length() > 1)
+    {
+        if (s.length() < 2)
+        {
+            cout << (player == 1 ? "No" : "Yes") << endl;
+            exit(0);
+        }
+        if (s.length() == 2)
+        {
+            if (player == 1) cout << (s[0] == s[1] ? "Yes" : "No") << endl;
+            else cout << (s[0] == s[1] ? "No" : "Yes") << endl;
+            exit(0);
+        }
+
         int pi = -1;
-        for (int i = 0; i < s.length() - 1; i++){
-            if (s[i] == s[i+1]){
-                if (i == 0){
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (s[i] == s[i + 1])
+            {
+                if (i == 0)
+                {
                     s = s.substr(2);
                     player = 2;
-                } else if (i+1 == s.length() - 1){
+                }
+                else if (i + 1 == s.length() - 1)
+                {
                     s = s.substr(0, s.length() - 2);
-                } else {
-                    if (pi == -1){
+                }
+                else
+                {
+                    if (pi == -1)
+                    {
                         pi = i;
-                    } else if (pi != -1 && s[pi - 1] != s[pi + 2]){
+                    }
+                    else if (pi != -1 && s[pi - 1] != s[pi + 2])
+                    {
                         pi = i;
                     }
                 }
             }
         }
-        if (pi == -1) break;
+        if (pi == -1)
+            break;
         s.erase(pi, 2);
         player = player == 1 ? 2 : 1;
     }
-    cout << (player == 1 ? "Yes" : "No") << endl;
+    cout << (player == 2 ? "Yes" : "No") << endl;
     return 0;
 }
