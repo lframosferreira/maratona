b9534c2ee1af85705ac27a5abc858ec38bfa99f5
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-08-04 11:52:10 -0300

    corringindo o template

--- STL/phoenixandbeauty.cpp
+++ STL/phoenixandbeauty.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define endl '\n';
-#define f first;
-#define s second;
-#define pb push_back();
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back()
 
 typedef long long ll;
 typedef pair<int , int> ii;
@@ -14,23 +14,30 @@ typedef pair<int , int> ii;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+int isBeautiful(const vector<int> &v, int k){
+    int beautiful = 1;
+    int firstSum = reduce(v.begin(), v.begin() + k);
+    for (int  i = 0; i < (int)v.size() - k; i++){
+        int currSum = firstSum - v[0] + v[k + i];
+        if (currSum != firstSum) {
+            beautiful = 0; break;
+        }
+    }
+    return beautiful;
+}
+
 int main(){ _
-    int t;cin>>t;
+    int t; cin>>t;
     int n, k;
     while (t--){
         cin>>n>>k;
         vector<int> v(n);
-        for (int i = 0; i < n; i++){
-            cin>>v[i];
-        }
-        int beautiful = 0;
-        int firstSum = reduce(v.begin(), v.begin() + k);
-        for (int  i = 0; i < n - k; i++){
-            int currSum = firstSum - v[0] + v[n - k + i + 1];
-            if (currSum != firstSum) {
-                beautiful = 1; break;
+        for (int i = 0; i < n; i++) cin >> v[i];
+        if (isBeautiful(v, k)) {
+            cout << n << endl;
+            for (int a = 0; a < n; a++){
+                cout << v[a] << (a != n - 1 ? ' ' : endl);
             }
-
         }
     }
     exit(0);
--- temp.cpp
+++ temp.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define endl '\n';
-#define f first;
-#define s second;
-#define pb push_back();
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back()
 
 typedef long long ll;
 typedef pair<int , int> ii;
