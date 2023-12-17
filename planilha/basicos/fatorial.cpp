929577e22b993e56dc994161739435d28cf2eb08
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-12-17 19:14:01 -0300

    fatorial

--- planilha/basicos/fatorial.cpp
+++ planilha/basicos/fatorial.cpp
@@ -1,16 +1,28 @@
+// pescoço pra baixo é canela
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    int n; cin >> n;
-    int res = 1;
-    while (n > 0)
-    {
-        res *= n;
-        n--;
-        
-    }
-    cout << res;
-};
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+
+typedef long long ll;
+typedef pair<int , int> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+  ll n;
+  int ans;
+  int c = 1;
+  while (cin >> n){
+    cout << "Instancia " << i << endl;
+    cout << ans << endl;
+  }
+    exit(0);
+}
