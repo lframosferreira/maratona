a16177504c16d9fa2afdb0d8da4982edc560ca76
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-02-18 23:06:23 -0300

    algms bem dificeis

--- progcomp/2532.cpp
+++ progcomp/2532.cpp
@@ -25,22 +25,27 @@ int main(){ _
     int N, P;
     while (cin >> N >> P){
         vector<pii> v(N+1);
+        int max_dmg = 0;
         for (int i = 0; i < N; i++){
             int D, M; cin >> D >> M;
             v[i] = {D, M};
+            max_dmg+=D;
         }
-        vector<vi> dp(N+1, vi(P+1, INF));
+        vector<vi> dp(N+1, vi(max_dmg+1, INF));
+        for (int i = 0; i <= N; i++) dp[i][0] = 0;
         for (int i = 1; i <= N; i++){
-            for (int j = 1; j <= P; j++){
-                dp[i][j] = dp[i-1][j];
+            for (int j = 1; j <= max_dmg; j++){
                 if (v[i-1].f <= j){
-                    dp[i][j] = min(dp[i][j], dp[i-1][j - v[i-1].f] + v[i-1].s);
+                    dp[i][j] = min(dp[i-1][j], dp[i-1][j - v[i-1].f] + v[i-1].s);
+                } else {
+                    dp[i][j] = dp[i-1][j];
                 }
-                else dp[i][j] = 0;
             }
         }
-        
-        cout << dp[N][P] << endl;
+        int ans = INF;
+        for (int i = P; i <=max_dmg; i++) ans=min(ans, dp[N][i]);
+        if (ans == INF) cout << -1 << endl;
+        else cout << ans << endl;
     }
     exit(0);
 }
