d07d535d3114c5c098022cb9d37a670b8ce03e27
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-03-13 20:41:09 -0300

    começando e2

---
+++ contests/progcomp/1/e2.cpp
@@ -0,0 +1,32 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+const int MAX = 2e5 + 10;
+vector<int> g[MAX];
+
+int main(){ _
+    int n, m; cin >> n >> m;
+     
+    exit(0);
+}
