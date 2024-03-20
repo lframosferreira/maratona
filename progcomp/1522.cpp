21b7ebd8f2b933261971e975408ed3a01af23bf1
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-03-20 10:29:46 -0300

    focar em outras coisas agr

---
+++ progcomp/1522.cpp
@@ -0,0 +1,39 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    while (1){
+        int N; cin >> N;
+        if (N==0) break;
+        vector<vector<int>> pilhas(3, vector<int>(N));
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < 3; j++){
+                cin >> pilhas[j][i];
+            }
+        }
+        int p0=0,p1=0,p2=0;
+
+    }    
+    exit(0);
+}
