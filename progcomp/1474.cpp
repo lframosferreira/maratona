aca687420d87a8c6c9bf7e28411ef9a1403fd93d
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-03-22 14:27:16 -0300

    socorr

--- progcomp/1474.cpp
+++ progcomp/1474.cpp
@@ -23,12 +23,39 @@ typedef vector<ll> vll;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
-const int MOD = 1e6;
+const ll MOD = 1e6;
+const ll MAX = 1e9+10; // nao é, arrumar dps
+
+ll fat[MAX];
+
+ll fexp(ll a, ll b){
+    a %=MOD;
+    ll res=1;
+    while (b>0){
+        if (b&1) res *= a%MOD;
+        a*=a%MOD;
+        b>>=1;
+    }
+    return res;
+}
 
 int main(){ _
     ll N, K, L;
+    fat[0]=fat[1]=1;
+    for (int i = 2; i < MAX; i++) fat[i]=fat[i-1]*i%MOD;
     while (cin >> N >> K >> L){
-            
+        ll n10=0;
+        ll n5;
+        ll ans=0;
+        while (n10*10 <= N){
+            ll val10 = fexp(L, n10);  
+            n5 = (N-n10*10)/5;
+            ll val5=fexp(K, n5);
+            ll nperm = fat[n10+n5]/(fat[n10]*fat[n5]);
+            ans+=(nperm*val5%MOD*val10%MOD);
+            n10++;
+        }
+        cout << setfill('0') << setw(6) << ans%MOD << endl;
     }
     exit(0);
 }
