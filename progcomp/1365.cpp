6831910a44793c03da1627ef55df6922f6fd4403
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-03-22 12:14:03 -0300

    é

---
+++ progcomp/1365.cpp
@@ -0,0 +1,35 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    while (1){
+        int R, C, K; cin >> R >> C >> K;
+        if (R==0 and C==0 and K==0) break;
+        vector<string> v(R);
+        for (int i = 0; i < R; i++) cin >> v[i];
+        vector<vector<int>> dp(R, vector<int>(C));
+    }
+    exit(0);
+}
--- progcomp/1522.cpp
+++ progcomp/1522.cpp
@@ -30,10 +30,21 @@ int main(){ _
         for (int i = 0; i < N; i++){
             for (int j = 0; j < 3; j++){
                 cin >> pilhas[j][i];
+                pilhas[j][i] %= 3;
             }
         }
         int p0=0,p1=0,p2=0;
+        while (1){
+            if (p0==N and p1==N and p2==N){
+                cout << 1 << endl;
+                exit(0);
+            }
 
+            while (p0<N-1 and v[0][p0]==0) p0++;
+            while (p1<N-1 and v[1][p1]==0) p1++;
+            while (p2<N-1 and v[2][p2]==0) p2++;
+            if (
+        }
     }    
     exit(0);
 }
