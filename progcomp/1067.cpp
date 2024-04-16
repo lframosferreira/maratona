d45538759029054114459ee4bd32aa7efc2de424
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-04-16 13:37:18 -0300

    facil

---
+++ progcomp/1067.cpp
@@ -0,0 +1,32 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<vd> vdd;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    int x; cin >> x;
+    for (int i = 1; i <= x; i+=2) cout << i << endl;    
+    exit(0);
+}
