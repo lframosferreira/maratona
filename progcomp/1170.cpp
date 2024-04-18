4efa88bbf8e1abe8a7337bee50952e2e01a05eb1
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-04-18 16:57:36 -0300

    aaa

---
+++ progcomp/1170.cpp
@@ -0,0 +1,40 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<vd> vdd;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    int n; cin >> n;
+    while (n--){
+        double c; cin >> c;
+        int cnt=0;
+        while (c>1){
+            cnt++;
+            c/=2.0;
+        }
+        cout << cnt << " dias" << endl;
+    }    
+    exit(0);
+}
