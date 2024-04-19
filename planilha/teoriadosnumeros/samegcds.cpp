530d7a726d149c3b2a5d01f9a985469c553339cf
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-04-19 13:47:34 -0300

    tentando

---
+++ planilha/teoriadosnumeros/samegcds.cpp
@@ -0,0 +1,38 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef vector<vd> vdd;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    int t; cin >> t;
+    while (t--){
+        ll a, m; cin >> a >> m;
+        ll val = gcd(a, m);
+        ll cnt=1;
+        for (ll i = 1; i < m; i++) if (val%i==0)cnt++;
+    cout << cnt << endl;    
+    }
+    exit(0);
+}
