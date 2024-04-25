1adc9cb64bbf64d44a4151a265471b21bc338afb
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-04-25 16:29:11 -0300

    aaa

--- progcomp/2898.cpp
+++ progcomp/2898.cpp
@@ -21,18 +21,59 @@ typedef vector<ll> vll;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+int N, M, A;
+
+const int MAX = 60;
+
+struct Edge {
+    int to;
+    int w;
+    int rev_idx;
+};
+
+int src, tgt;
+pii par[MAX];
+int gargalo;
+bool vis[MAX];
+vector<vector<Edge>> g;
+
+bool bfs(int u){
+    queue<int> q;
+    q.push(u);
+    vis[u]=true;
+    while (!q.empty()){
+        auto v=q.front();q.pop();
+        for (int i = 0; i < g[v].size(); i++){
+            auto edg=g[v][i];
+            if (edg.w <=0) continue;
+            if (vis[edg.to]) continue;
+            vis[edg.to]=true;
+            if (edg.to==tgt){
+                par[edg.to]={v, i};
+                return true;
+            }
+            par[edg.to]={v, i};
+            q.push(edg.to);
+        }
+    }
+    return false;
+}
+
 int main(){ _
     while (true){
-        int N, M, A; cin >> N >> M >> A; 
+        cin >> N >> M >> A; 
         if (N == M == A == 0) break;
-        vector<vector<pii>> g(N+1);
         while (M--){
             int O, D, S;
             cin >> O >> D >> S;
-            g[O].pb({D, S});
-            g[D].pb({O, S});
+            g[O].pb({D, S, g[D].size()});
+            g[D].pb({O, 0, g[O].size()-1});
+
+            g[D].pb({O, S, g[O].size()});
+            g[O].pb({D, 0, g[D].size()-1});
         }
         
+
         
     }
     cout << endl;
