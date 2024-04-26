1049aa7b1c20c14f2a03ac153422c4ee30bb1dc2
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-04-25 21:32:20 -0300

    tentando

--- progcomp/1394.cpp
+++ progcomp/1394.cpp
@@ -22,41 +22,30 @@ typedef vector<ll> vll;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+struct Edge{
+    int to;int w; int rev_idx;
+};
+
+vector<vector<Edge>> g;
+int games[MAX][MAX];
+int pnts[MAX];
+
 int main(){ _
     while (1){
         int N, M, G; cin >>N >> M >> G;
         if(N==0) break;
-        vector<vector<int>> g(N, vector<int>(N, M*2)); 
-        vector<int> pnts(N);
+        memset(pnts, 0, sizeof pnts);
+        memset(games, 0, sizeof games);
+        g.clear();g.resize(N+1);
         while (G--){
             char r; int i, j;
             cin >> i >>r>> j;
             if (r=='<'){
                 pnts[j]+=2;
-                g[j][i]-=2;
-                g[i][j]-=2;
             } else {
                 pnts[i]++;pnts[j]++;
-                g[j][i]-=2;
-                g[i][j]-=2;
             }
         }
-        for (int i = 1;i <N;i++){
-            pnts[0]+=g[0][i];
-            g[i][0]=0;
-        }
-        for (int i = 1;i < N; i++){
-            int maybe=pnts[i];
-            for (int j = 1;j < N;j++){
-                if(i==j) continue;
-                maybe+=g[i][j]; 
-            }
-            if (maybe >= pnts[0]){
-                can=false;break;
-            }
-        }
-        if(can) cout << "Y" <<endl;
-        else cout << "N" << endl;
     } 
     exit(0);
 }
