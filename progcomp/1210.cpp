25b10f02de56b0c37aadcbd7f8ae1ec26b985056
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-05-07 22:48:44 -0300

    vodka

--- progcomp/1210.cpp
+++ progcomp/1210.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #define dbg(x) cout << #x << " = " << x << endl
 
 typedef long long ll;
-typedef pair<int , int> pii;
+typedef pair<int , int> ii;
 typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
@@ -34,32 +34,46 @@ int main(){ _
         vi custo(M); for (int i = 0; i < M; i++) cin >> custo[i];
         vi venda(M+1); for (int i = 1; i <= M; i++) cin >> venda[i];
         vvi dp(N+1, vi(M+1));
-        for (int i = 1; i <= M; i++){
-            dp[N][i] = -venda[i];
+        vvi vendeu(N+1, vi(M+1));
+        dp[N][M]=-venda[M]+P+custo[0];
+        vendeu[N][M]=1;
+        for (int i = 0; i < M; i++){
+            int x = -venda[i]+custo[0]+P;
+            int y = custo[i];
+            if (x<= y){
+                vendeu[N][i]=1;
+                dp[N][i]=x;
+            }else {
+                dp[N][i]=y;
+            }
         }
-        for (int i = 1; i < N; i++){
-            for (int j = 1; j < M; j++){
-                if (j==M){
-                    dp[i][j]=dp[i+1][1]-venda[M];
-                    continue;
-                }
+        for (int i = N-1; i>=1; i--){
+            for (int j = M; j >= 1; j--){
                 // vendi
-                int x = dp[i+1][1] - venda[i];
+                int y =INF;
+                int x = dp[i+1][1] - venda[j] + custo[0]+P;
                 // nao vendi
-                int y = dp[i+1][j+1] + custo[i];
+                if (j < M) y = dp[i+1][j+1] + custo[j];
                 if (x <= y){
+                    vendeu[i][j]=1;
                     dp[i][j]=x;
                 }else {
                     dp[i][j]=y;
                 }
             }
         }
-        for (int i = 1; i <= N; i++){
-            for (int j = 1; j <= M; j++){
-                cout << dp[i][j] << " ";
-            }
-            cout << endl;
+        cout << dp[1][I] << endl;
+        int mudou_algm_vez=0;
+        for(int a = 1, b=I;a <=N; a++){
+            if (vendeu[a][b]){
+                if (mudou_algm_vez) cout << " ";
+                mudou_algm_vez=1;
+                cout << a;
+                b=1;// checamos desde o começo
+            }else b++;
         }
+        if (!mudou_algm_vez) cout << 0;
+        cout << endl;
     } 
     exit(0);
 }
