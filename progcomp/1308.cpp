16debb7e5b83a4f3c445bc0de85ae95ee02f265c
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-05-27 16:00:18 -0300

    to melhorando

---
+++ progcomp/1308.cpp
@@ -0,0 +1,49 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define sz(v) (int)v.size()
+#define f first
+#define s second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define umap unordered_map
+#define uset unordered_set
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int , int> ii;
+typedef tuple<int, int, int> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    int t; cin >> t;
+    while (t--){
+        ull n; cin >> n;
+        ull l=1,r=1e18;
+        while (l<r){
+            ull m = l + (r-l)/2;
+            if (m > (2*n)/(m+1)) r=m;
+            else l = m+1;
+        }
+        cout << l-1 << endl;
+    }    
+    exit(0);
+}
