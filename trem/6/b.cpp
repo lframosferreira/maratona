c2df4b7b413f14a5764d2647fd71fcc4d79d75f6
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-06-02 21:33:28 -0300

    algumas do trem 6

---
+++ trem/6/b.cpp
@@ -0,0 +1,69 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define sz(v) (int)v.size()
+#define f first
+#define s second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define bs bitset
+#define umap unordered_map
+#define uset unordered_set
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> ii;
+typedef tuple<int, int, int> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    int t; cin >> t;
+    while (t--){
+        int n; cin >> n;
+        vi v(n);for (int &i : v) {cin >> i;i--;}
+        string bs; cin >> bs;
+        int idx=0;
+        bool bad = false;
+        for (int i = 0; i < n; i++){
+            char c = bs[i];
+            if (bad) break;
+            if (c=='1'){
+                while (v[idx] != i){
+                    idx++;
+                    if (idx == n) {
+                        bad=true;
+                        break;
+                    }
+                }
+            } else {
+                if (v[idx] == i) {
+                    if (idx == n-1){
+                        bad=true;
+                        break;
+                    }
+                }
+                else idx++;
+            }
+        }
+        if (bad) cout << "NO\n";
+        else cout << "YES\n";
+    }
+    exit(0);
+}
---
+++ trem/6/c.cpp
@@ -0,0 +1,53 @@
+// pescoço pra baixo é canela
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define sz(v) (int)v.size()
+#define f first
+#define s second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define umap unordered_map
+#define uset unordered_set
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int , int> ii;
+typedef tuple<int, int, int> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int v[30];
+
+int main(){ _
+    string a, b;
+    memset(v, 0, sizeof v);
+    cin >> a >> b;
+    for (auto &c: a) v[c-'a']++;
+    bool bad=false;
+    for (auto &c: b){
+        if (c=='*') continue;
+        if (v[c-'a'] == 0){
+            bad=true;
+            break;
+        }
+        v[c-'a']--;
+    }
+    cout << (bad ? "N": "S") << endl;
+    exit(0);
+}
