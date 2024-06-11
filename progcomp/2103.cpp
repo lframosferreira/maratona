b8a3a1c5f22705bbd9707652a6cfe7252abb74a2
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-06-11 13:06:56 -0300

    modifiquei temp.cpp

--- progcomp/2103.cpp
+++ progcomp/2103.cpp
@@ -12,6 +12,7 @@ using namespace std;
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
+#define bs bitset
 #define umap unordered_map
 #define uset unordered_set
 #define all(v) begin(v), end(v)
@@ -33,7 +34,41 @@ typedef vector<vd> vvd;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+const int MAX = 1e4+10;
+const int MOD=1300031;
+
+vector<vector<ii>> g;
+ull ans;
+int d[MAX];
+
+void dfs(int u, int x){
+    if (d[u] != -1) return;
+    ans+=x%MOD;
+    d[u]=x;
+    for (auto [a, b] : g[u]) {
+        if (d[a] != -1) continue;
+        dfs(a, x+b);
+    }
+}
+
 int main(){ _
-    
+    int N;
+    int t; cin >> t;
+    while (t--){
+        cin >> N;
+        g.clear();
+        g.resize(N+1);
+        for (int i = 0; i < N-1; i++){
+            int u, v, w; cin >> u >> v >> w;
+            g[u].pb({v, w});
+            g[v].pb({u, w});
+        }
+        ans=0LL;
+        for (int i = 1; i <= N; i++){
+            for (int j =0; j < MAX; j++) d[j]=-1;
+            dfs(i, 0);            
+        }
+        cout << ans/2 << endl;
+    }    
     exit(0);
 }
