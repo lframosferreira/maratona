4ea24b4a9e570fd347248fbabd8dc2890e79a482
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-06-21 18:17:10 -0300

    corrigindo uma bobagem no quiz universitario

--- progcomp/2555.cpp
+++ progcomp/2555.cpp
@@ -47,16 +47,12 @@ int main(){ _
         vd chance(N+1);
         for (int i = 1; i <= N; i++) cin >> chance[i];
         vvd dp(N+1, vd(K+1));
-        vvd acumulada(N+1, vd(K+1, 1.0));
         for (int i = 0; i <= K; i++){
             dp[N][i] = i > 0 ? premios[N] : premios[N]*(chance[N]/100.0);
-            acumulada[N][i] = i > 0 ? 1.0 : chance[N]/100.0;
         }
         dp[N][0]=premios[N] * (chance[N]/100.0);
-        acumulada[N][0] = chance[N]/100.0;
         for (int i = N-1; i >= 0; i--){
             dp[i][0]=(premios[i] + dp[i+1][0])*(chance[i]/100.0);
-            acumulada[i][0]=acumulada[i+1][0] * (premios[i]/100.0);
         }
         for (int i = N-1; i >= 1; i--){
             for (int j = K; j >= 1; j--){
@@ -64,10 +60,8 @@ int main(){ _
                 double y = (chance[i]/100.0)*dp[i+1][j] + premios[i]*(chance[i]/100.0);
                 if (y >= x){
                     dp[i][j] = y;
-                    acumulada[i][j] = (chance[i] / 100.0) *acumulada[i+1][j];
                 }else {
                     dp[i][j]=x;
-                    acumulada[i][j] = acumulada[i+1][j-1];
                 }
             }
         }
