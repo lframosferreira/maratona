286d4a579edbe67f2a4d0319820249ee05bc19ff
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-06-24 20:36:37 -0300

    tentando 2291

--- progcomp/2291.cpp
+++ progcomp/2291.cpp
@@ -12,7 +12,6 @@ using namespace std;
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
-#define bs bitset
 #define umap unordered_map
 #define uset unordered_set
 #define all(v) begin(v), end(v)
@@ -21,6 +20,7 @@ using namespace std;
 #define dbg(x) cout << #x << " = " << x << endl
 
 typedef long long ll;
+typedef unsigned long long ull;
 typedef pair<int , int> ii;
 typedef tuple<int, int, int> iii;
 typedef vector<int> vi;
@@ -33,10 +33,34 @@ typedef vector<vd> vvd;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
-const int MAX = 1e6 + 10;
-int divi[MAX];
+const int MAX = 1e6+10;
+ull d[MAX];
+ull divi[MAX];
+
+void crivo(){
+   for (ull i = 1; i < MAX; i++) divi[i]=1;
+   for (ull i = 2; i < MAX  ; i++){
+    if (divi[i]==1) for (ull j = i; j < MAX; j+=i){
+        divi[j]=i;
+    }
+   }
+}
 
 int main(){ _
-    
+    ull n;
+    crivo();
+    d[1]=1;
+    d[2]=3;
+    d[3]=4;
+    for (ull i = 4; i < MAX; i++){
+        if (divi[i]==i) d[i]=i+1;
+        else d[i] = d[i/divi[i]] + i;
+    }
+    for (ull i = 2; i < MAX; i++) d[i]+=d[i-1];
+    while (true){
+        cin >> n;
+        if (n==0) break;
+        cout << d[n] << endl;
+    }    
     exit(0);
 }
