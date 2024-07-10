aa30e3897789de40a3b89203ae07d7cc4151c45a
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-07-10 12:36:43 -0300

    azedou

--- progcomp/2669.cpp
+++ progcomp/2669.cpp
@@ -38,32 +38,38 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 using cd = complex<double>;
 const double PI = acos(-1);
 
-void fft(vector<cd> &a, bool invert) {
-  int n = a.size();
-  if (n == 1)
-    return;
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
 
-  vector<cd> a0(n / 2), a1(n / 2);
-  for (int i = 0; 2 * i < n; i++) {
-    a0[i] = a[2 * i];
-    a1[i] = a[2 * i + 1];
-  }
-  fft(a0, invert);
-  fft(a1, invert);
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
 
-  double ang = 2 * PI / n * (invert ? -1 : 1);
-  cd w(1), wn(cos(ang), sin(ang));
-  for (int i = 0; 2 * i < n; i++) {
-    a[i] = a0[i] + w * a1[i];
-    a[i + n / 2] = a0[i] - w * a1[i];
-    if (invert) {
-      a[i] /= 2;
-      a[i + n / 2] /= 2;
+        if (i < j)
+            swap(a[i], a[j]);
     }
-    w *= wn;
-  }
-}
 
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if (invert) {
+        for (cd & x : a)
+            x /= n;
+    }
+}
 vector<int> multiply(vector<int> const &a, vector<int> const &b) {
   vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
   int n = 1;
